53c0bbd86cfad15f89fa369e6c16b64f1f2b397f
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-05-08 16:53:57 +0900

    Cpp_Expert_201
    Cpp_Expert_201

--- Cpp_Expert/Cpp_Expert.cpp
+++ Cpp_Expert/Cpp_Expert.cpp
@@ -1,5 +1,5 @@
 ﻿#include <bits/stdc++.h>
-// 현재 페이지 : 83
+// 현재 페이지 : 201
 using namespace std;
 
 #pragma region Chapter 00.템플릿
@@ -20,7 +20,7 @@ using namespace std;
 
 #pragma region 질문 리스트
 /*
-
+1장 : 
     1. C++ 에서 빌드는 어떻게 이루어지는가?
     2. 런타임, 컴파일 타임?
     3. 스택과 힙
@@ -30,7 +30,25 @@ using namespace std;
     7. 예외 처리
     8. 타입 추론 auto
     9. decltype
+    10. 생성자 이니셜라이저
+    11. 유니폼 초기화
 
+2장 :
+    1. 스트링 리터럴
+    2. 로 스트링 리터럴
+    3. 메모리 영역(코드, 데이터, 스택, 힙)
+    4. string_view
+
+3장 :
+    1. 주석 작성법
+
+4장 :
+    1. 추상화
+    2. 재사용
+    3. 빅-오 표기법
+
+5장 :
+    1. has-a 관계, is-a 관계
 
 */
 #pragma endregion
