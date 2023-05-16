cbcbf9f2d94f4f3cfb5c1c2c628c2167366af238
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-05-16 15:58:43 +0900

    [unrated] Title: 배열의 원소만큼 추가하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for(auto& v : arr)
+    {
+        for(int i = 0; i<v; i++)
+        {
+            answer.push_back(v);
+        }
+    }
+    return answer;
+}
