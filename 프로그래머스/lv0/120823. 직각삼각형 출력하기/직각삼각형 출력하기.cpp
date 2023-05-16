1fe86f8db87336a75d757741fccc53024908e005
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-05-16 15:58:54 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 1.58 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++)
+    {
+        cout << string(i, '*') << endl;
+    }
+    
+    return 0;
+}
