ac44a244e15dd43c2bc24402505bfe41ea4c4c39
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-06-07 17:49:52 +0900

    [unrated] Title: 덧칠하기, Time: 0.13 ms, Memory: 5.02 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -4,21 +4,20 @@ using namespace std;
 
 int solution(int n, int m, vector<int> section) 
 {
-    int answer = 0;
-    vector<int> vi(n + 1, true); 
-
-    for (auto& v : section)
-        vi[v] = false;
-
-    for (int i = 1; i <= n; i++) 
+    int answer = 1;
+    
+    int start = section[0];
+    
+    for(auto& v : section)
     {
-        if (!vi[i]) 
+        if(v < start + m)
+            continue;
+        else
         {
-            for (int j = i; j <= i + m - 1 && j <= n; j++) 
-                vi[j] = true;
-
-            answer++; 
+            start = v;
+            answer++;
         }
     }
+    
     return answer;
 }
