b016a24129ddc6df0ce61cfc2492b44fbd007321
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-11-22 10:10:22 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[31] = {0, };
+    int a = 0;
+    
+    for(int i = 0; i<28; i++)
+    {
+        cin >>a;
+        arr[a] = 1;
+    }
+    
+    for(int i = 1; i<=30; i++)
+    {
+        if(!arr[i])
+            cout<< i << "\n";
+    }
+    
+    return 0;
+}
