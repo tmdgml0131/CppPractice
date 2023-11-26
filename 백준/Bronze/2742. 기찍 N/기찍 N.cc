8fb4acc88e6cb382c909ff29c3af742bfc098409
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-11-26 15:28:45 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 첫째줄 시 분 초
+// 둘째줄 초단위
+// 출력 시 분 초 단위로 언제 끝나는지
+int main()
+{
+    int a;
+    cin >> a;
+    
+    for(a; a>=1; --a)
+    {
+        cout << a << "\n";
+    }
+    return 0;
+}
