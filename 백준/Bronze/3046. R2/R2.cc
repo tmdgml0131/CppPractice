d71798ff42f8ab80cbc3f010fa5346aa37d97c60
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-11-27 09:20:57 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int answer = 0;
+    cin >> a >> b;
+    
+    answer = (2*b) - a;
+    
+    cout << answer;
+    
+    return 0;
+}
