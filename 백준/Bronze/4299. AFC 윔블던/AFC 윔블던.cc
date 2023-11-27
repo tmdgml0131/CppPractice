01e119ec7e9ea14900925b6563bb452cd6310b82
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-11-27 09:53:47 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, x, y;
+    cin>>a>>b;
+    if(a < b)
+        cout<<"-1";
+    else{
+        x=(a+b)/2;
+        y=(a-b)/2;
+        if(x+y==a && x-y==b)
+            cout<<x<<" "<<y;
+        else
+            cout<<"-1";
+    }
+    return 0;
+}
