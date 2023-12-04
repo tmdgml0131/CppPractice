8e65ad0f66ca2c6cd2b6046fce5f7b7f32b743bc
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-12-04 10:12:25 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+        int a, b;
+        cin >> a >> b;
+        int t = a+b;
+        if(t == 0)
+            break;
+        cout << t << '\n';
+    }
+    
+    
+    return 0;
+}
