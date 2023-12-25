f0a520a2bdc721428686f67d62fd70f04719eab5
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-12-25 14:21:46 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    while(a--)
+    {
+        int tmp;
+        cin >> tmp;
+        while(tmp--)
+        {
+            cout << "=";
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
