d33309379a5f75de7f0df999297684a5de7e824b
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2023-12-27 01:09:45 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    
+    cout << fixed;
+    cout.precision(6);
+    
+    cout << (2*a) + (2*3.141592*b);
+}
+    
